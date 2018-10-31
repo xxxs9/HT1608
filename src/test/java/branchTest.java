559ab724ab9121250899b65bf162f9a1cf295d4f
'xxxs9' <'751723378@qq.com'>
2018-10-31 15:37:59 +0800

    master update 1

--- src/test/java/branchTest.java
+++ src/test/java/branchTest.java
@@ -1,4 +1,11 @@
 
 public class branchTest {
 
+	public static void test() {
+		System.out.println("master update");
+	}
+	
+	public static void test2() {
+		System.out.println("master update2");
+	}
 }
