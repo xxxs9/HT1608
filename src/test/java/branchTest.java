b73b1b4ad8ff332c1835fa25108fe2da56e46f5f
'xxxs9' <'751723378@qq.com'>
2018-10-31 16:15:03 +0800

    master function

--- src/test/java/branchTest.java
+++ src/test/java/branchTest.java
@@ -8,4 +8,8 @@ public class branchTest {
 	public static void test2() {
 		System.out.println("master update2");
 	}
+	
+	public static void master() {
+		System.out.println("master have update2");
+	}
 }
