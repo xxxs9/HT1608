7095674f1163434b052bb19d987e83e423e86962
'xxxs9' <'751723378@qq.com'>
2018-10-31 16:34:25 +0800

    create master test3

--- src/test/java/branchTest.java
+++ src/test/java/branchTest.java
@@ -16,4 +16,8 @@ public class branchTest {
 	public static void master() {
 		System.out.println("master have update2");
 	}
+	
+	public static void test3() {
+		System.out.println("master test3");
+	}
 }
