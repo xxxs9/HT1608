d2d485e56a4e108a022dfa8e88a17040293257ea
xxxs9 <40586703+xxxs9@users.noreply.github.com>
2018-11-02 19:13:06 +0800

    Update BranchTest.java

--- src/test/java/BranchTest.java
+++ src/test/java/BranchTest.java
@@ -1,7 +1,9 @@
 
 public class BranchTest {
 
-	
+	public void masterTest4(){
+		System.out.println("master test one");
+	}	
 	
 	public void masterTest1(){
 		System.out.println("master test one");
